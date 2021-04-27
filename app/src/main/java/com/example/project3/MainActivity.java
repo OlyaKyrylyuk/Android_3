11e880912d0dbac8168e67450a54d2ff2f70ff75
OlyaKyrylyuk <olya_kyrylyuk15@ukr.net>
2021-04-28 01:02:08 +0300

    added LR5

--- app/src/main/java/com/example/project3/MainActivity.java
+++ app/src/main/java/com/example/project3/MainActivity.java
@@ -5,12 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.content.res.Configuration;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.PopupMenu;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -25,6 +30,8 @@ public class MainActivity extends AppCompatActivity {
         final EditText Email = findViewById(R.id.email);
         final EditText Password = findViewById(R.id.password);
         final Button sendDataButton = findViewById(R.id.sendDataButton);
+        ImageView popup = findViewById(R.id.popup);
+        popup.setOnClickListener(viewClickListener);;
         final Button changeButton = findViewById(R.id.change);
         sendDataButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -49,6 +56,12 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+    View.OnClickListener viewClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            showPopupMenu(v);
+        }
+    };
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
@@ -83,6 +96,51 @@ public class MainActivity extends AppCompatActivity {
                     setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         }
         return true;
+    }
+
+    private void showPopupMenu(View v) {
+        PopupMenu popupMenu = new PopupMenu(this, v);
+        popupMenu.inflate(R.menu.popup);
+
+        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+                        switch (item.getItemId()) {
+                            case R.id.next_page1:
+                                Intent intent = new Intent(MainActivity.this, ResultActivity.class);
+                                startActivity(intent);
+
+                                return true;
+                            case R.id.change_text1:
+                                final EditText Surname = findViewById(R.id.surname);
+                                final EditText Name = findViewById(R.id.name);
+                                final EditText Email = findViewById(R.id.email);
+                                final EditText Password = findViewById(R.id.password);
+                                Surname.setText("Kyrylyuk");
+                                Name.setText("Olya");
+                                Email.setText("olya@gmail.com");
+                                Password.setText("1111");
+
+                                return true;
+                            case R.id.toast:
+                                Toast toast = Toast.makeText(getApplicationContext(),
+                                        "One of the options from menu - Toast with the violet picture",
+                                        Toast.LENGTH_SHORT);
+                                toast.setGravity(Gravity.CENTER, 0, 0);
+                                LinearLayout toastContainer = (LinearLayout) toast.getView();
+                                ImageView tostImageView = new ImageView(getApplicationContext());
+                                tostImageView.setImageResource(R.drawable.picture);
+                                toastContainer.addView(tostImageView,0);
+                                toast.show();
+                                return true;
+
+                            default:
+                                return false;
+                        }
+                    }
+                });
+
+        popupMenu.show();
     }
 
 }
--- app/src/main/java/com/example/project3/ResultActivity.java
+++ app/src/main/java/com/example/project3/ResultActivity.java
@@ -6,9 +6,16 @@ import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.PopupMenu;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class ResultActivity extends AppCompatActivity {
 
@@ -21,6 +28,8 @@ public class ResultActivity extends AppCompatActivity {
         TextView nameInfo = (TextView)findViewById(R.id.nameInfo);
         TextView emailInfo = (TextView)findViewById(R.id.emailInfo);
         TextView passwordInfo = (TextView)findViewById(R.id.passwordInfo);
+        ImageView popup = findViewById(R.id.popup);
+        popup.setOnClickListener(viewClickListener);;
         String surname =  getIntent().getStringExtra("Surname");
         String name =  getIntent().getStringExtra("Name");
         String email =  getIntent().getStringExtra("Email");;
@@ -31,6 +40,12 @@ public class ResultActivity extends AppCompatActivity {
         emailInfo.setText("Email "+ email);
         passwordInfo.setText("Password "+password);
     }
+    View.OnClickListener viewClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            showPopupMenu(v);
+        }
+    };
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
@@ -67,6 +82,50 @@ public class ResultActivity extends AppCompatActivity {
 
         }
         return true;
+    }
+    private void showPopupMenu(View v) {
+        TextView surnameInfo = (TextView)findViewById(R.id.surnameInfo);
+        TextView nameInfo = (TextView)findViewById(R.id.nameInfo);
+        TextView emailInfo = (TextView)findViewById(R.id.emailInfo);
+        TextView passwordInfo = (TextView)findViewById(R.id.passwordInfo);
+        PopupMenu popupMenu = new PopupMenu(this, v);
+        popupMenu.inflate(R.menu.popup);
+
+        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.next_page1:
+                        Intent intent = new Intent(ResultActivity.this, MainActivity.class);
+                        startActivity(intent);
+
+                        return true;
+                    case R.id.change_text1:
+                        surnameInfo.setText("Surname: "+ "Kyrylyuk");
+                        nameInfo.setText("Name " + "Olya");
+                        emailInfo.setText("Email "+ "olya@gmail.com");
+                        passwordInfo.setText("Password "+"1111");
+
+                        return true;
+                    case R.id.toast:
+                        Toast toast = Toast.makeText(getApplicationContext(),
+                                "One of the options from menu - Toast with the violet picture",
+                                Toast.LENGTH_SHORT);
+                        toast.setGravity(Gravity.CENTER, 0, 0);
+                        LinearLayout toastContainer = (LinearLayout) toast.getView();
+                        ImageView tostImageView = new ImageView(getApplicationContext());
+                        tostImageView.setImageResource(R.drawable.picture);
+                        toastContainer.addView(tostImageView,0);
+                        toast.show();
+                        return true;
+
+                    default:
+                        return false;
+                }
+            }
+        });
+
+        popupMenu.show();
     }
 
 }
